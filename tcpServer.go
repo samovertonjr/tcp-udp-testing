5e59483da913cd5c8b2dd40e28d773d72f4991f0
Samuel Overton Jr <samovertonjr@gmail.com>
2022-02-15 07:18:59 -0600

    add tcp client and server

---
+++ tcpServer.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	arguments := os.Args
+
+	if len(arguments) == 1 {
+		fmt.Println("Please provide port number.")
+	}
+
+	PORT := ":" + arguments[1]
+	listener, err := net.Listen("tcp", PORT)
+
+	if err != nil { 
+		fmt.Println(err)
+		return
+	}
+
+	defer listener.Close()
+
+	client, err := listener.Accept()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		netData, err := bufio.NewReader(client).ReadString('\n')
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if strings.TrimSpace(string(netData)) == "STOP" {
+			fmt.Println("Exiting TCP server!")
+			return
+		}
+
+		fmt.Print("-> ", string(netData))
+
+		t := time.Now()
+
+		myTime := t.Format(time.RFC3339) + "\n"
+
+		client.Write([]byte(myTime))
+	}
+
+}
